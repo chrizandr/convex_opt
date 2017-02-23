8604f2dafcbc3b47c45ad99c4072c75994a2b98d
Chris Andrew <chris.g14@iiits.in>
2017-02-23 19:34:30 +0530

    Solutions for assignment 1

---
+++ assignment1/q1.py
@@ -0,0 +1,29 @@
+from cvxopt import matrix, solvers
+import pdb
+'''
+The following inequalities are to be used while computing the function min{ 0(x1) + 0(x2) }
+-1(x1) + 0(x2) <= 0;
+0(x1) + -1(x2) <= 0;
+-1(x1) + -1(x2) <= -1;
+1(x1) + 1(x2) <= 2;
+-1(x1) + -2(x2) <= -2;
+'''
+# inequalities equations
+A = matrix([
+        [-1.0 , 0.0, -1.0, 1.0, -1.0 ],
+        [0.0 , -1.0, -1.0, 1.0, -2.0 ]
+          ])
+# bounds
+B = matrix([ 0.0,
+             0.0,
+            -1.0,
+             2.0,
+            -2.0 ])
+# Cost function
+C = matrix([ 0.0,
+             0.0 ])
+
+sol = solvers.lp(C,A,B)
+print( 'The solution for the inequalities is')
+print( 'x1' , sol['x'][0])
+print( 'x2' , sol['x'][1])
